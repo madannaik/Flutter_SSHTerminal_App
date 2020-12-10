b7fc2fb090a8063983598c89435ee22d13a7a3e3
madannaik <48375643+madannaik@users.noreply.github.com>
2020-12-10 18:47:04 +0530

    Update loginScreen.dart

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -219,9 +219,7 @@ Route _signUpRoute() {
       var begin = Offset(0.0, 1.0);
       var end = Offset.zero;
       var curve = Curves.ease;
-
       var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
       return SlideTransition(
         position: animation.drive(tween),
         child: child,
